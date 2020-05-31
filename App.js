f12261667e08a1f07506b43c382a3423f2cdb783
Santiago Bonessi <santiagobonessi@hotmail.com>
2020-05-31 21:45:12 +1000

    add styles to stack headers

--- App.js
+++ App.js
@@ -1,13 +1,86 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { createAppContainer, NavigationContext } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+const Logo = () => {
+  return (
+  <Image 
+  source={require('./assets/sils_icon.png')}
+  style={styles.image}
+  >
+  </Image>
+  );
+}
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Home Screen!</Text>
+      <Button
+        title='Go to detail'
+        onPress={() => navigation.navigate('Detail', { name: 'Santiago', user_id: '10' })}
+      ></Button>
     </View>
   );
 }
+
+HomeScreen.navigationOptions = {
+  title: 'Main',
+  headerTitle: <Logo/>,
+  headerStyle: {
+    backgroundColor: '#333',
+  },
+}
+
+const DetailScreen = ({ navigation }) => {
+  const userName = navigation.getParam('name', 'default value');
+  return (
+    <View style={styles.container}>
+      <Text>Detail Screen!</Text>
+      <Text>User Name: {userName}</Text>
+      <Button
+        title='USER NAME'
+        onPress={() => navigation.setParams({ title: 'User: Santiago' })}
+      ></Button>
+    </View>
+  );
+}
+
+DetailScreen.navigationOptions = ({ navigation, navigationOptions }) => {
+  return {
+    title: navigation.getParam('title', 'Loading...'),
+    headerStyle: {
+      backgroundColor: navigationOptions.headerStyle.backgroundColor,
+    },
+  }
+}
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Detail: {
+      screen: DetailScreen,
+    }
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#bbb',
+      },
+      headerTintColor: '#ffe',
+      headerTitleStyle: {
+        fontSize: 30,
+        fontWeight: '400'
+      }
+    }
+  }
+)
+
+export default createAppContainer(AppNavigator)
 
 const styles = StyleSheet.create({
   container: {
@@ -16,4 +89,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    height: 40,
+    width: 40,
+  }
 });
